76210fcfaf56bda56996e3b18cab8b44829b8c00
Jinny <asdfghjk1052@naver.com>
2025-02-14 18:58:13 +0900

    Update update_blog.py

--- scripts/update_blog.py
+++ scripts/update_blog.py
@@ -4,7 +4,7 @@ import os
 
 # 벨로그 RSS 피드 URL
 # example : rss_url = 'https://api.velog.io/rss/@rimgosu'
-rss_url = 'https://api.velog.io/rss/@BBooong'
+rss_url = 'https://api.velog.io/rss/@so356hot'
 
 # 깃허브 레포지토리 경로
 repo_path = '.'
